61c3faf189212971f77296e12e5a5f0e8f4a3b35
nemopss <alexey_gladilin@mail.ru>
2024-06-11 15:57:24 +0300

    Add flag to choose between storage options

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"log"
 	"net/http"
 
 	_ "github.com/lib/pq"
+	"github.com/nemopss/go-posts-comments-system/internal/repository"
 	"github.com/nemopss/go-posts-comments-system/internal/repository/inmemory"
+	"github.com/nemopss/go-posts-comments-system/internal/repository/postgres"
 	"github.com/nemopss/go-posts-comments-system/internal/server"
 )
 
 func main() {
-	rep := inmemory.NewInMemoryRepository()
+	postgresStorageFlag := flag.Bool("postgres", false, "A flag to determine which storage you want to use. true -> Postgres, false -> in-memory storage")
+	flag.Parse()
+	var rep repository.Repository
+	if !*postgresStorageFlag {
+		rep = inmemory.NewInMemoryRepository()
+	} else {
+		connStr := ""
+		db, err := sql.Open("postgres", connStr)
+		if err != nil {
+			log.Fatalf("Error opening db: %v", err)
+		}
+		rep = postgres.NewPostgresRepository(db)
 
+	}
 	srv := server.NewServer(rep)
 	http.Handle("/graphql", srv.Handler())
 
